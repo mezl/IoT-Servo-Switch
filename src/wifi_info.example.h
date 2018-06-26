404cb1afffb97fc9ea6d28ac407cf4c96d7860d8
Kai@Pixelbook Linux <kai@aiorobotics.com>
2018-06-26 09:28:42 +0800

    update example wifi setting with sinric api key

---
+++ src/wifi_info.example.h
@@ -0,0 +1,9 @@
+#define WIFI_SSID "Your SSID"
+#define WIFI_PASS "Your wifi password"
+#define MySSID WIFI_SSID
+#define MyWifiPassword WIFI_PASS
+
+// TODO: Change to your sinric API Key. Your API Key is displayed on
+// sinric.com dashboard 
+#define MyApiKey "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx" 
+#define DORMLIGHT_ID "XXXX"
